24eb5684d88ee512fc970acfb814d3fdf5b9824f
leninner <mazabandalenin180@gmail.com>
2021-08-09 17:24:19 -0500

    Añado el challenge con promesas y una carpeta utils

--- src/promises/index.js
+++ src/promises/index.js
@@ -14,5 +14,22 @@ somethingWillHappen()
 
 const somethingWillHappen2 = () => {
     //NOTE: Promise se debe escribir con mayúscula siempre
-    return new Promise((resolve, reject) => {});
+    return new Promise((resolve, reject) => {
+        if (true) {
+            setTimeout(() => {
+                resolve("True");
+            }, 2000);
+        } else {
+            const error = new Error("Whooops!");
+            reject(error);
+        }
+    });
 };
+
+somethingWillHappen2()
+    .then((response) => console.log(response))
+    .catch((err) => console.error(err));
+
+Promise.all([somethingWillHappen(), somethingWillHappen2()])
+    .then((response) => console.log("Array of results: ", response))
+    .catch((err) => console.error(err));
